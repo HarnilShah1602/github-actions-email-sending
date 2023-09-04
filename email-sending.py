01b30e3ebe15bab5d14317964add24b37eda0958
harnil <harnilshah1602@gmail.com>
2023-09-04 18:38:24 +0530

    Modified the e-mail files

--- email-sending.py
+++ email-sending.py
@@ -1,20 +1,31 @@
-import smtplib, ssl
+import smtplib
 import os
+from sre_constants import SUCCESS
+import ssl
 
-port = 465
-smtp_server = "smtp.gmail.com"
-USER_EMAIL = os.environ.get("USER_EMAIL")
-USER_PASSWORD = os.environ.get("USER_PASSWORD")
+def lambda_handler(event, context):
+    port = 465
+    smtp_server = "smtp.gmail.com"
+    USER_EMAIL = os.environ.get("USER_EMAIL")
+    USER_PASSWORD = os.environ.get("USER_PASSWORD")
 
-message = """\
-    Subject: Welcome Ubaydah
+    message = """\
+    Subject: Welcome Harnil
 
     This is your welcome email running 
-"""
+    """
 
-context = ssl.create_default_context()
+    context = ssl.create_default_context()
 
-server = smtplib.SMTP_SSL(smtp_server, port, context=context)
+    server = smtplib.SMTP_SSL(smtp_server, port, context=context)
 
-server.login(USER_EMAIL, USER_PASSWORD)
-server.sendmail(USER_EMAIL, USER_EMAIL, message)
+    server.login(USER_EMAIL, USER_PASSWORD)
+    server.sendmail(USER_EMAIL, USER_EMAIL, message)
+    
+    return {
+        "status": 200,
+        "body": SUCCESS
+    }
+
+
+
